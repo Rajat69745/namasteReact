b8bcec7fd4d07e956b136d8442be66879bfd1ec9
rajat <rajwahi97@gmail.com>
2023-09-22 13:55:23 +0530

    updated

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading= React.createElement('h5',
+
+{id:'heading', alpha:'abc'}
+,'namaste react'
+)
+
+console.log(heading)
+const root= ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(heading)
