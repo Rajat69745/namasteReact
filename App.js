4d016e2a7082590344ad8a509e917f32ad774ac4
rajat <rajwahi97@gmail.com>
2023-09-28 10:42:14 +0530

    updated

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 
 import React from 'react'
-import { ReactDOM } from 'react'
+import  ReactDOM  from 'react-dom/client'
 const heading= React.createElement('h1',{id:'heading', alpha:'abc'}
-,'namaste op'
+,'namaste oaap'
 )
 
 console.log(heading)
